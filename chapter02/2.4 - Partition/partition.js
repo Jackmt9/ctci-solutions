bc7d071ac9310b6f968cdd7a074cda3709372136
jackmt9 <jackmt9@gmail.com>
2021-03-14 15:59:01 -0400

    updated 2.4

--- chapter02/2.4 - Partition/partition.js
+++ chapter02/2.4 - Partition/partition.js
@@ -1,6 +1,6 @@
 // Partition: Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x. If x is contained within the list, the values of x only need to be after the elements less than x (see below). The partition element x can appear anywhere in the "right partition"; it does not need to appear between the left and right partitions.
 // EXAMPLE
-// Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1[partition=5] 
+// Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition=5] 
 // Output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
 
 
@@ -11,9 +11,69 @@ class LinkedList {
     }
 }
 
+// O(n) time | O(n) space?
+// function partition(head, value) {
+//     let list1Head = null
+//     let list1Tail = null
 
-function partition(head) {
+//     let list2Head = null
+//     let list2Tail = null
 
+//     let node = head
+
+//     while (node !== null) {
+
+//         node.next = null
+
+//         if (node.value < value) {
+//             if (list1Head === null) {
+//                 list1Head = node
+//                 list1Tail = list1Head
+//             } else {
+//                 list1Tail.next = node
+//                 list1Tail = node
+//             }
+//         } else {
+//             if (list2Head === null) {
+//                 list2Head = node
+//                 list2Tail = list2Head
+//             } else {
+//                 list2Tail.next = node
+//                 list2Tail = node
+//             }
+//         }
+//         node = node.next
+//     }
+
+//     if (list1Head === null) return list2Head
+
+//     list1Tail.next = list2Head
+
+//     return list1Head
+// }
+
+// O(n) time | O(n) space?
+function partition(head, value) {
+    let mainHead = head
+    let tail = head
+
+    let node = head
+    while (node !== null) {
+        const next = node.next
+        if (node.value < value) {
+            // Insert node at head
+            node.next = mainHead
+            mainHead = node
+        } else {
+            // Insert node at tail
+            tail.next = node
+            tail = node
+        }
+        node = next
+    }
+    tail.next = null
+
+    return mainHead
 }
 
 
--- chapter02/2.4 - Partition/partition.test.js
+++ chapter02/2.4 - Partition/partition.test.js
@@ -1,3 +1,20 @@
 const LinkedList = require('./partition').LinkedList
 const partition = require('./partition').partition
 
+// Test Error!
+test('List = 5 -> 3 -> 2, k = 2', () => {
+    const head = new LinkedList(5)
+    head.next = new LinkedList(3)
+    head.next.next = new LinkedList(2)
+   
+    const headCopy = new LinkedList(5)
+    headCopy.next = new LinkedList(3)
+    headCopy.next.next = new LinkedList(2)
+
+    const expectedLinkedList = new LinkedList(2)
+    expectedLinkedList.next = new LinkedList(5)
+    expectedLinkedList.next.next = new LinkedList(3)
+
+    expect(partition(head, 2)).toEqual(expectedLinkedList)
+    expect(head).toEqual(headCopy)
+})
