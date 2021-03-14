eea7af780456f88a99046b1c2d5739a310e96bb3
jackmt9 <jackmt9@gmail.com>
2021-03-13 23:02:17 -0500

    1.6 solution and test

--- chapter01/1.6 - String Compression/stringCompression.js
+++ chapter01/1.6 - String Compression/stringCompression.js
@@ -1,7 +1,21 @@
 // String Compression: Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed"string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a - z).
 
+// O(n) time | O(n) space
 function compressString(string) {
+    const array = []
+    let freqCount = 0
 
+    for (let i = 0; i < string.length; i++) {
+        freqCount++
+        if (string[i] !== string[i + 1] || i === string.length - 1) {
+            array.push(string[i], freqCount)
+            freqCount = 0
+        }
+    }
+
+    const compressed = array.join('')
+    return string.length < compressed.length ? string : compressed
 }
+
 
 module.exports = compressString
--- chapter01/1.6 - String Compression/stringCompression.test.js
+++ chapter01/1.6 - String Compression/stringCompression.test.js
@@ -1,5 +1,17 @@
 const compressString = require('./stringCompression')
 
-test('', () => {
-    expect(compressString()).toBe()
+test('string = "aabcccccaaa"', () => {
+    expect(compressString("aabcccccaaa")).toBe("a2b1c5a3")
+})
+
+test('string = "aabcccaaddac"', () => {
+    expect(compressString("aabcccaaddac")).toBe("aabcccaaddac")
+})
+
+test('string = "abbbcddddd"', () => {
+    expect(compressString("abbbcddddd")).toBe("a1b3c1d5")
+})
+
+test('string = "abc"', () => {
+    expect(compressString("abc")).toBe("abc")
 })
