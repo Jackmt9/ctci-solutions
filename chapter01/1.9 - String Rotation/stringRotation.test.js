fb36d57054da81cb1c751d1df74d109823c2501f
jackmt9 <jackmt9@gmail.com>
2021-03-14 11:36:58 -0400

    tests added

--- chapter01/1.9 - String Rotation/stringRotation.test.js
+++ chapter01/1.9 - String Rotation/stringRotation.test.js
@@ -3,3 +3,19 @@ const isSubstring = require('./stringRotation')
 test('s1 = "waterbottle", s2 = "erbottlewat"', () => {
     expect(isSubstring("waterbottle", "erbottlewat")).toBe(true)
 })
+
+test('s1 = "test", s2 = "ttes"', () => {
+    expect(isSubstring("test", "ttes")).toBe(true)
+})
+
+test('s1 = "melon", s2 = "lemon"', () => {
+    expect(isSubstring("melon", "lemon")).toBe(false)
+})
+
+test('s1 = "apple", s2 = "eapple"', () => {
+    expect(isSubstring("apple", "eapple")).toBe(false)
+})
+
+test('s1 = "eapple", s2 = "apple"', () => {
+    expect(isSubstring("eapple", "apple")).toBe(false)
+})
---
+++ chapter02/2.1 - Remove Dups/removeDups.js
@@ -0,0 +1,16 @@
+// Remove Dups: Write code to remove duplicates from an unsorted linked list. FOLLOW UP
+// How would you solve this problem if a temporary buffer is not allowed?
+
+class LinkedList {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+function removeDups(head) {
+    
+}
+
+exports.LinkedList = LinkedList
+exports.removeDups = removeDups
